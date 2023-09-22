86b4adb82cb3023ceeef379cf12e74d7cdc415f3
padmed <ddarchinian@gmail.com>
2023-09-22 21:20:15 +0400

    Redirect client on country change

--- src/components/CountrySelect.js
+++ src/components/CountrySelect.js
@@ -1,13 +1,26 @@
 import { useSelector } from "react-redux";
 import { selectMappedCountries } from "../reducers/selectors";
+import { useNavigate } from "react-router-dom";
 
 const CountrySelect = () => {
   const countries = useSelector(selectMappedCountries);
+  const navigate = useNavigate();
+
+  const handleSelectChange = (e) => {
+    const selectedOption = e.target.options[e.target.selectedIndex];
+    const countryCode = selectedOption.getAttribute("data-country-code");
+    navigate(`/${countryCode}`);
+  };
 
   return (
-    <select>
+    <select onChange={handleSelectChange}>
+      <option value="" hidden>
+        Choose the country
+      </option>
       {countries.map((country) => (
-        <option key={country.code}>{country.name}</option>
+        <option key={country.code} data-country-code={country.code}>
+          {country.name}
+        </option>
       ))}
     </select>
   );
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>,
 );
 
 // If you want to start measuring performance in your app, pass a function

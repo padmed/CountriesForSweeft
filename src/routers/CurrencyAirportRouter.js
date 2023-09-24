e175f3cbaf9dd71d155266ec5501983784f6ac57
David Darchinian <98668534+padmed@users.noreply.github.com>
2023-09-24 14:44:31 +0000

    Refactor code

--- src/routers/CurrencyAirportRouter.js
+++ src/routers/CurrencyAirportRouter.js
@@ -6,20 +6,23 @@ import CurrencyExchange from "../components/CurrencyExchange";
 const CurrencyAirportRouter = () => {
     const selectedCountry = useSelector((state) => state.selectedCountry);
     const path = useLocation();
-    return (<>
-          {selectedCountry &&
-        selectedCountry.cca3 === path.pathname.split("/")[1] && (
-          <Routes>
-            <Route
-              path={`/${selectedCountry.cca3}/airports`}
-              element={<Airports />}
-            />
-            <Route
-              path={`/${selectedCountry.cca3}`}
-              element={<CurrencyExchange />}
-            />
-          </Routes>
-        )}</>)
+
+    if (!selectedCountry || selectedCountry.cca3 !== path.pathname.split("/")[1]) {
+      return null; // Render nothing if the conditions are not met
+    }
+    
+    return <Routes>
+              <Route
+                path={`/${selectedCountry.cca3}/airports`}
+                element={<Airports />}
+              />
+              <Route
+                path={`/${selectedCountry.cca3}`}
+                element={<CurrencyExchange />}
+              />
+            </Routes>
+
+
 }
 
 export default CurrencyAirportRouter

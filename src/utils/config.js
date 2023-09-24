c712db31cce4759357526ccd1bcbaaa0f05c9b33
padmed <ddarchinian@gmail.com>
2023-09-25 02:41:30 +0400

    Add API keys

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,6 @@
 const config = {
-  API_KEY: "AIzaSyDbMFlVJeJ_sJBCWsrgEcgO01SfaQYVRe4",
+  API_KEY_LOCATION: "AIzaSyDbMFlVJeJ_sJBCWsrgEcgO01SfaQYVRe4",
+  API_KET_AIRPORTS: "CJktCxqU4ZUk8ye8ftLYWg==HdfQez7GILMPAYgq",
 };
 
 export default config;

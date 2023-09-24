664e39cec33d17ba4bd3f468c3f4d49e7a37ad93
padmed <ddarchinian@gmail.com>
2023-09-25 02:42:20 +0400

    Fetch cca2

--- src/services/countries.js
+++ src/services/countries.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const baseUrl =
-  "https://restcountries.com/v3.1/all?fields=name,flags,cca3,capital,currencies,region,continents,population,borders";
+  "https://restcountries.com/v3.1/all?fields=name,flags,cca3,cca2,capital,currencies,region,continents,population,borders";
 
 const getAll = async () => {
   const request = await axios.get(baseUrl);

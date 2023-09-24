fea7364ed3a5cbe4fc2986abda765a4e0330d93c
padmed <ddarchinian@gmail.com>
2023-09-24 13:36:09 +0400

    Add custom selectors

--- src/reducers/selectors.js
+++ src/reducers/selectors.js
@@ -11,3 +11,32 @@ export const selectMappedCountries = createSelector(
     }));
   },
 );
+
+export const selectMappedCountriesWithCurrency = createSelector(
+  [selectCountriesData],
+  (countries) => {
+    return countries.map((country) => {
+      // console.log(country);
+      const currencyInfo =
+        country.currencies && Object.values(country.currencies)[0];
+      return {
+        name: country.name.common,
+        currency: currencyInfo ? Object.keys(country.currencies)[0] : "N/A",
+        currencyName: currencyInfo ? currencyInfo.name : "N/A",
+        currencySymbol: currencyInfo ? currencyInfo.symbol : "N/A",
+      };
+    });
+  },
+);
+
+export const selectBaseCurrency = createSelector(
+  [(state) => state.selectedCountry.currencies],
+  (currencies) => {
+    const currency = Object.values(currencies);
+    return {
+      currency: currency.length > 0 ? Object.keys(currencies)[0] : "N/A",
+      currencyName: currency.length > 0 ? currency[0].name : "N/A",
+      currencySymbol: currency.length > 0 ? currency[0].symbol : "N/A",
+    };
+  },
+);

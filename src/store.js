ccc36ec8afbb2315b4373da053de21383abe474b
padmed <ddarchinian@gmail.com>
2023-09-25 11:48:24 +0400

    Add reducer

--- src/store.js
+++ src/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import countriesReducer from "./reducers/countriesReducer";
 import selectedCountryReducer from "./reducers/selectedCountryReducer";
 import ratesReducer from "./reducers/ratesReducer";
+import airportsReducer from "./reducers/airportsReducer";
 
 const store = configureStore({
   reducer: {
     countries: countriesReducer,
     selectedCountry: selectedCountryReducer,
     rates: ratesReducer,
+    airports: airportsReducer,
   },
 });
 

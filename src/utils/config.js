7d76526a9bfe3e83f91fac243618c90b2b63775c
padmed <ddarchinian@gmail.com>
2023-09-25 18:45:26 +0400

    Add comment

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,4 @@
+// API keys are not hidden for a purpose
 const config = {
   API_KEY_LOCATION: "AIzaSyDbMFlVJeJ_sJBCWsrgEcgO01SfaQYVRe4",
   API_KET_AIRPORTS: "CJktCxqU4ZUk8ye8ftLYWg==HdfQez7GILMPAYgq",
